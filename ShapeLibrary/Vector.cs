3eb67415118111a02cf9aa344df7c23d8e0b8ba0
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-02 08:48:20 -0400

    add operator overloads for +-*/ == !=

--- ShapeLibrary/Vector.cs
+++ ShapeLibrary/Vector.cs
@@ -22,6 +22,33 @@ namespace ShapeLibrary
             Y = v.Y;
         }
 
-    
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + b.X, a.Y + b.Y);
+        }
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X - b.X, a.Y - b.Y);
+        }
+        public static Vector operator *(float scalar, Vector a)
+        {
+            return new Vector(scalar *  a.X, scalar * a.Y);
+        }
+        public static Vector operator /(float scalar, Vector a)
+        {
+            return new Vector(a.X / scalar, a.Y / scalar); 
+        }
+        public static bool operator ==(Vector a, Vector b)
+        {
+            return (a.X == b.X) && (a.Y == b.Y);
+        }
+        public static bool operator !=(Vector a, Vector b)
+        {
+            if (a.X != b.X || a.Y != b.Y) {  return true; }
+            return false;
+        }
+
+
+
     }
 }
