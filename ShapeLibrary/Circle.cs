5774d173805bf7407c7cd74fad0fd52a1a7bd11f
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-09 09:05:00 -0400

    make Circle class implement ICircle

---
+++ ShapeLibrary/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class Circle : ICircle
+    {
+        private float _x;
+        private float _y;
+        private float _radius;
+        private Colour _colour;
+
+        public Circle(float x, float y, float radius, Colour colour)
+        {
+            _x = x;
+            _y = y;
+            _radius = radius;
+            _colour = colour;
+        }
+        public float Radius => throw new NotImplementedException();
+
+        public Vector Center => throw new NotImplementedException();
+
+        public Vector[] Vertices => throw new NotImplementedException();
+
+        public Colour Colour => throw new NotImplementedException();
+    }
+}
