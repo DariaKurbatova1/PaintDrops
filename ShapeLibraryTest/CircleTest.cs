8806391fd27750ef26b7d11471600c6cb63322b6
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-20 14:49:51 -0400

    change circle unit tests

--- ShapeLibraryTest/CircleTest.cs
+++ ShapeLibraryTest/CircleTest.cs
@@ -15,28 +15,31 @@ namespace ShapeLibraryTest
         public void InitializesCorrectly()
         {
             Colour colour = new Colour(122, 122, 122);
-            Rectangle rectangle = new Rectangle(0, 0, 10, 5, colour);
-            Assert.AreEqual(0, rectangle.X);
-            Assert.AreEqual(0, rectangle.Y);
-            Assert.AreEqual(10, rectangle.Width);
-            Assert.AreEqual(5, rectangle.Height);
-            Assert.AreEqual(colour, rectangle.Colour);
+            Circle circle = new Circle(0, 0, 10, colour);
+            Assert.AreEqual(0, circle.Center.X);
+            Assert.AreEqual(0, circle.Center.Y);
+            Assert.AreEqual(10, circle.Radius);
+            Assert.AreEqual(colour, circle.Colour);
         }
 
         [TestMethod]
         public void CalculateVerticesTest()
         {
             Colour colour = new Colour(122, 122, 122);
-            Rectangle rectangle = new Rectangle(0, 0, 10, 5, colour);
-            var expectedVertices = new Vector[]
-            {
-                new Vector(0, 0),
-                new Vector(10, 0),
-                new Vector(10, 5),
-                new Vector(0, 5)
-            };
+            Circle circle = new Circle(0, 0, 10, colour);
+            var expectedVertices = new Vector[100];
+            const float PI = (float)Math.PI;
 
-            var vertices = rectangle.Vertices;
+            // Calculate expected vertices
+            for (int i = 0; i < 100; i++)
+            {
+                float theta_i = (2 * PI / 100) * i;
+                float x_i = 10 * (float)Math.Cos(theta_i);
+                float y_i = 10 * (float)Math.Sin(theta_i);
+                expectedVertices[i] = new Vector(x_i, y_i);
+            }
+
+            var vertices = circle.Vertices;
 
             Assert.AreEqual(expectedVertices.Length, vertices.Length);
             for (int i = 0; i < expectedVertices.Length; i++)
@@ -49,8 +52,8 @@ namespace ShapeLibraryTest
         public void ColourPropertyTest()
         {
             Colour colour = new Colour(122, 122, 122);
-            Rectangle rectangle = new Rectangle(0, 0, 10, 5, colour);
-            Assert.AreEqual(colour, rectangle.Colour);
+            Circle circle = new Circle(0, 0, 10, colour);
+            Assert.AreEqual(colour, circle.Colour);
         }
     }
 }
