935e3b10b91bc30b3a74e1f3b1c86ac7becd0acc
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-21 09:03:47 -0400

    add marble method

---
+++ PaintDropSimulation/PaintDrop.cs
@@ -0,0 +1,31 @@
+﻿using ShapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintDropSimulation
+{
+    internal class PaintDrop : IPaintDrop
+    {
+        public ICircle Circle { get; set; }
+
+        public void Marble(IPaintDrop other)
+        {
+            Vector[] newVertices = new Vector[Circle.Vertices.Length];
+            //apply the algorithm to all vertices
+            var otherCenter = other.Circle.Center;
+            var otherRadius = other.Circle.Radius;
+            int index = 0;
+            foreach (var vertice in Circle.Vertices)
+            {
+                var squareRoot = Math.Sqrt((1 + ((otherRadius * otherRadius) / Vector.magnitude(vertice - otherCenter));
+                var newVertice = otherCenter + (float)squareRoot * (vertice - otherCenter);
+                newVertices[index] = newVertice;
+                index++;
+            }
+        }
+
+    }
+}
