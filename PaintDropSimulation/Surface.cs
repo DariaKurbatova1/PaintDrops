f0f326fd4b164f5ac0c940929324917677ced87d
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-11-11 11:12:45 -0500

    connect the surface to pattern generation and modify user interactions

--- PaintDropSimulation/Surface.cs
+++ PaintDropSimulation/Surface.cs
@@ -11,6 +11,7 @@ namespace PaintDropSimulation
     {
 
         public Surface(int width, int height) {
+
             Width = width;
             Height = height;
             Drops = new List<IPaintDrop>();
@@ -19,12 +20,18 @@ namespace PaintDropSimulation
         public int Width { get; }
 
         public int Height { get; }
+        public Vector posisition = new Vector();
 
         public List<IPaintDrop> Drops { get; }
         public void GeneratePaintDropPattern(float radius, Colour colour)
         {
-            Vector? position = PatternGeneration.Invoke(this);
-            AddPaintDrop(new PaintDrop(new Circle(position.Value.X, position.Value.Y, radius, colour)));
+            
+            Vector? position = PatternGeneration?.Invoke(this);
+            if (position.HasValue)
+            {
+                AddPaintDrop(new PaintDrop(new Circle(position.Value.X, position.Value.Y, radius, colour)));
+
+            }
         }
 
         public void AddPaintDrop(IPaintDrop drop)
--- PatternGenerationLib/PhyllotaxisPatternGeneration.cs
+++ PatternGenerationLib/PhyllotaxisPatternGeneration.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace PatternGenerationLib
 {
-    internal class PhyllotaxisPatternGeneration : IPatternGenerator
+    public class PhyllotaxisPatternGeneration : IPatternGenerator
     {
         public const double GOLDEN_ANGLE = 137.5;
         public double A = Math.PI / 180 * GOLDEN_ANGLE;
         public int C = 10; //scaling factor c
         public int N = 100; //number of points to place
         public int Index = 0;
-        public Vector CalculatePatternPoint(ISurface surface)
+        public Vector? CalculatePatternPoint(ISurface surface)
         {
             if(Index < N)
             {
@@ -25,13 +25,12 @@ namespace PatternGenerationLib
                 //calculate one point
                 double ri = C * Math.Sqrt(Index);
                 double theta_i = Index * A;
-                double xi = xs * ri * Math.Cos(theta_i);
+                double xi = xs + ri * Math.Cos(theta_i);
                 double yi = ys + ri * Math.Sin(theta_i);
-                increaseIndex();
+                Index++;
                 return new Vector((float)xi, (float)yi);
             }
             return null;
         }
-        public void increaseIndex() { Index++; }
     }
 }
