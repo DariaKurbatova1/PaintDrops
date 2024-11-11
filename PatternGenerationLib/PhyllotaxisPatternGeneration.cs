eb226a4e5552c4e2ce8185e68513835620326fb3
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-11-11 08:47:27 -0500

    add PhyllotaxisPatternGeneration class

---
+++ PatternGenerationLib/PhyllotaxisPatternGeneration.cs
@@ -0,0 +1,19 @@
+﻿using PaintDropSimulation;
+using ShapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternGenerationLib
+{
+    internal class PhyllotaxisPatternGeneration : IPatternGenerator
+    {
+        public Vector? CalculatePatternPoint(ISurface surface)
+        {
+
+            return null;
+        }
+    }
+}
